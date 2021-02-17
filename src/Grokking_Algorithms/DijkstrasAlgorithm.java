956eb49923b3e5c1725b30687c7ed87a1ebbac7e
Evgeny Lyozin <evgenylyozindev@gmail.com>
2021-02-17 20:09:01 +0300

    Жадные алгоритмы, покрытие множества

--- src/Grokking_Algorithms/DijkstrasAlgorithm.java
+++ src/Grokking_Algorithms/DijkstrasAlgorithm.java
@@ -1,9 +1,6 @@
 package Grokking_Algorithms;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DijkstrasAlgorithm {
     // the graph
